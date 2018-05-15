0b03fde1f658e37eaa304ab4977a6dde287cae41
vvignesh17 <nityavignesh@gmail.com>
2018-05-15 23:37:12 +0530

    git merge branch 1

---
+++ sortingnum.cpp
@@ -0,0 +1,47 @@
+/* Bubble sort code */
+ 
+#include<stdio.h>//Iyer 110117101
+#include<stdlib.h>
+ 
+//bubble sort using function
+ 
+void bubble_sort(int [], int); //function definition
+ 
+int main() {
+    int array[100], n, c, d, swap;
+ 
+    printf("Enter number of elements\n");
+    scanf("%d", &n);
+ 
+    printf("Enter %d integers\n", n);
+ 
+    for (c = 0; c < n; c++)
+        scanf("%d", &array);
+ 
+    bubble_sort(array, n);
+ 
+    printf("Sorted list in ascending order:\n");
+ 
+    for (c = 0; c < n; c++)
+        printf("%d\n", array);
+ 
+    return 0;
+}
+ 
+void bubble_sort(int list[], int n) //function body
+{
+    int c, d, t;
+ 
+    for (c = 0; c < (n - 1); c++) //outer loop
+    {
+        for (d = 0; d < n - c - 1; d++) //inner loop
+        {
+            if (list[d] > list[d + 1]) {
+                /* Swapping */
+ 
+                t = list[d]; //using temporary variable
+                list[d] = list[d + 1];
+                list[d + 1] = t;
+            }
+        }
+    }
